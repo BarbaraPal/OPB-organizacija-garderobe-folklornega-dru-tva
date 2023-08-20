69b67bd8aadb0ebcfbd94c15cf2dcacf9fbc399e
BarbaraPal <pal.barbi@gmail.com>
2023-08-20 20:08:18 +0200

    dodan db port v Repo povezavo

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import plotly.io as pio
 # privzete nastavitve
 SERVER_PORT = os.environ.get('BOTTLE_PORT', 8080)
 RELOADER = os.environ.get('BOTTLE_RELOADER', True)
-DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
+#DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
 
 repo = Repo()
 
